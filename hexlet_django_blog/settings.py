0ed3b420a0eae9cf4eaa45160d64ab3e9b7355c9
Denis Dyakonov <holosty@yandex.ru>
2025-09-10 21:56:09 +0300

    lesson 2

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My APPS
+    'hexlet_django_blog',
+    # Django APPS
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'hexlet_django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
